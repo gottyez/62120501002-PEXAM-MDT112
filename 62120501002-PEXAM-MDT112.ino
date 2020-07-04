635b96b6b9db85633855294f24d1b7f117330947
gottyez <got171143@gmail.com>
2020-07-04 14:09:09 +0700

    5. Complete

--- 62120501002-PEXAM-MDT112.ino
+++ 62120501002-PEXAM-MDT112.ino
@@ -1,10 +1,21 @@
 const int buzzer = 8;
 
+
+
 #include <Stepper.h>
 
 #define STEPS_PER_360_DEGREE 2038
 
 Stepper stepper(STEPS_PER_360_DEGREE, 12, 10, 9, 11);
+
+
+
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+Adafruit_SSD1306 display = Adafruit_SSD1306(128, 32, &Wire);
 
 void setup(){
   pinMode(buzzer, OUTPUT);
@@ -15,6 +26,7 @@ void setup(){
   Serial.begin(9600);
   Serial.println("Hello MDT!");
 
+
   stepper.setSpeed(16);
 
   Serial.println("CW 90");
@@ -24,6 +36,20 @@ void setup(){
   Serial.println("CCW 180");
   stepper.step(-STEPS_PER_360_DEGREE/2);
   delay(800);
+
+
+  display.begin(0x3C);
+
+  display.clearDisplay();
+  display.setCursor(35, 20);
+  display.setTextColor(1);
+  display.setTextSize(1);
+  display.println("Hello World");
+  display.display();
+  delay(1000);
+
+  display.clearDisplay();
+  display.display();
 }
 
 void loop(){
